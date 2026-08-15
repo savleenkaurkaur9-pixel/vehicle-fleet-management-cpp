414e26f713a5bc92d750a0bced6d4d3925a2be04
savleen kaur <savleenkaurkaur9@gmail.com>
2026-08-15 17:04:39 +0530

    phase 3:Added Car,Bike,truck subclasses with inheritance and virtual calculateRent()

--- main.cpp
+++ main.cpp
@@ -21,14 +21,52 @@ class vehicle
     {
         cout<<"vehicle removed:"<<name<<endl;
     }
+    virtual double calculateRent(int hours)
+    {
+        return rateperhour*hours;
+    }
     void showdetails()
     {
         cout<<"ID:"<<vehicleId<<"Name:"<<name<<"Rate/hour:"<<rateperhour<<"Available:"<<(isAvailable ? "yes" : "No")<<endl;
     }
 };
+class Car:public vehicle
+{
+public:
+Car(int Id,string vname,double rate):vehicle(Id,vname,rate)
+{}
+double calculateRent(int hours) override{
+    return rateperhour*hours+100;
+}
+};
+class Bike:public vehicle
+{
+public:
+Bike(int Id,string vname,double rate):vehicle(Id,vname,rate)
+{}
+double calculateRent(int hours) override{
+    return rateperhour*hours+100;
+}
+};
+class Truck:public vehicle{
+ public:
+Truck(int Id,string vname,double rate):vehicle(Id,vname,rate)
+{}
+double calculateRent(int hours) override{
+        return rateperhour*hours+300;
+    }
+
+};
 int main()
 {
-    vehicle v1(1,"honda city",150.0);
-    v1.showdetails();
+    Car c1(1,"Honda City",150.0);
+    Bike b1(2,"Royal Enfield",80.0);
+    Truck t1(3,"Tata 407",200.0);
+   c1.showdetails();
+   cout<<"car rent for 5 hours:"<<c1.calculateRent(5)<<endl;
+   b1.showdetails();
+   cout<<"bike rent for 5 hours:"<<b1.calculateRent(5)<<endl;
+   t1.showdetails();
+   cout<<"truck rent for 5 hours:"<<t1.calculateRent(5)<<endl;
     return 0;
 }
